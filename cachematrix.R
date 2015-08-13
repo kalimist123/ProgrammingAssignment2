4b508b2e0c3a6412a502fc9115da29a9a858d1c5
kalimist123 <kali.mist@gmail.com>
2015-08-13 09:30:20 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,72 +1,34 @@
-
 ## makeCacheMatrix returns functions to get or set a cached inverted Matrix
 makeCacheMatrix <- function(x = matrix()) {
-  # stores the cached value
-  # initialize to NULL
-  cachedMatrix <- NULL
   
-  # create the matrix in the working environment
+  cache <- NULL
   set <- function(y) {
+    # use '<<-' to assign a value to an object in an environment 
+    # outside the current environment. 
     x <<- y
-    cachedMatrix <<- NULL
+    cache <<- NULL
   }
-  
-  # get the value of the matrix
   get <- function() x
-  # store inverse in cache
-  setInverse <- function(inverse) cachedMatrix <<- inverse
-  # get the inverted matrix from cache
-  getInverse <- function() cachedMatrix
+  setInverse <- function(inverse) cache <<- inverse
+  getInverse <- function() cache
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
   
-  # return the created functions to the working environment
-  list(set = set, get = get,setInverse = setInverse,getInverse = getInverse)
 }
 
 
 ## cacheSolve produces the inverse of a matrix returned from makeCacheMatrix
 ## If the inverted matrix does not yet exist it is created and cached
 
-cacheSolve <- function(x, ...) {
-  # get the cached matrix
-  cachedMatrix <- x$getInverse()
-  
-  # return matrix if exists
-  if (!is.null(cachedMatrix)) {
+cacheSolve <- function(x, ...) { 
+  inverse<- x$getInverse()
+  if(!is.null(inverse)) {
     message("getting cached data")
-    
-    # display matrix in console
-    return(cachedMatrix)
+    return(inverse)
   }
+  data <- x$get()
+  # solve() function solves equation a %*% x = b for x, where b is a vector or matrix.
+  inverse<- solve(data, ...)
+  x$setInverse(inverse)
+  inverse
   
-  # matrix does not exist so create from makeCachedMatrix.get()
-  matrix <- x$get()
-  
-  # use try catch to handle exceptions
-  tryCatch( 
-  {
-    # solve() function solves equation a %*% x = b for x, where b is a vector or matrix.
-    cachedMatrix <- solve(matrix, ...)
-  },
-  error = function(e) 
-  {
-    message("Solving this matrix caused an Error:")
-    message(e)
-    
-    return(NA)
-  },
-  warning = function(w) 
-  {
-    message("Solving this matrix caused a Warning:")
-    message(w)
-    
-    return(NA)
-  },
-  finally = 
-  {
-    # set inverted matrix in cache
-    x$setMatrix(cachedMatrix)
-  } 
-  )
-  
-  return (cachedMatrix)
 }
